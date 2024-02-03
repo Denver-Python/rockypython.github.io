13a9e850d299b883a702be0cf73157d6ea8ec7d5
Hugh Brown <hughdbrown@yahoo.com>
2024-02-03 00:11:54 -0700

    Minor code reorg

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@
 from st_pages import Page, show_pages
 
 page_list = [
-    Page("home.py", "Home"),
-    Page("general.py", "General Python Learning Resources"),
-    Page("data_science.py", "Data Science and Machine Learning with Python"),
-    Page("biology.py", "Biology and Bioinformatics with Python"),
+    Page("pages/home.py", "Home"),
+    Page("pages/general.py", "General Python Learning Resources"),
+    Page("pages/data_science.py", "Data Science and Machine Learning with Python"),
+    Page("pages/biology.py", "Biology and Bioinformatics with Python"),
 ]
 
 show_pages(page_list)
